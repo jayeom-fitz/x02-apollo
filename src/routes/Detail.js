82d3c0a3d63be80ba5fb819025fe1b96b08d2b5e
JayEom <djawjdgus93@naver.com>
2020-12-08 19:21:34 +0900

    #1-2 GET MOVIES Query 2

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,10 +1,26 @@
 import React from 'react';
+import {useParams} from "react-router-dom";
+import {gql} from "apollo-boost";
+import { useQuery } from '@apollo/react-hooks';
+
+const GET_MOVIE = gql`
+  query getMovie($id: Int){
+    movie(id: $id){
+      id
+      title
+      medium_cover_image
+    }
+  }
+`;
 
 function Detail() {
+  const { id } = useParams();
+  const { loading, data } = useQuery(GET_MOVIE);
+  console.log(loading, data);
   return (
-      <div>
-          Detail
-      </div>
+    <div>
+      {id}
+    </div>
   );
 }
 
